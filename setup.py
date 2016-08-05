e4817a12f4bb33b3bd2c3ee9131d5f06db8f8282
xrotwang <xrotwang@googlemail.com>
2016-08-05 16:05:38 +0200

    bumped version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name='pyclpa',
-    version="0.2.0",
+    version="0.3.0",
     description='A python library to check phonetic transcriptions',
     long_description=read("README.md"),
     author='Johann-Mattis List',
